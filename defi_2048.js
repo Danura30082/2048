4c942cc00af37cb021b26d08c04b3b09e4c33d9c
Danura30082 <135959478+Danura30082@users.noreply.github.com>
2024-01-31 17:28:00 +0100

    defi 6

--- defi_2048.js
+++ defi_2048.js
@@ -1,8 +1,18 @@
 testbonjour();
-
+document.addEventListener('keyup', (event)=> {
+    keytester(event);
+});
 function bonjour(){
     console.log("Bonjour")
 }
 function testbonjour(){
     bonjour()
+}
+function haut(){
+    console.log("haut")
+}
+function keytester(event){
+    if (event.key == "ArrowUp"){
+        haut()
+    }
 }
