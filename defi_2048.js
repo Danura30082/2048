d57b0b52fbae0ec90fe29db0cefcf9b142d0101f
Danura30082 <135959478+Danura30082@users.noreply.github.com>
2024-01-31 17:29:08 +0100

    defi 9

--- defi_2048.js
+++ defi_2048.js
@@ -1,5 +1,5 @@
 testbonjour();
-
+var x = 0;
 document.addEventListener('keyup', (event)=> {
     keytester(event);
 });
@@ -24,21 +24,33 @@ function droite(){
 function keytester(event){
     if (event.key == "ArrowUp"){
         haut()
+        x = x + 1;
     }
     if (event.key == "ArrowDown"){
         bas()
+        x = x + 1;
     }
     if (event.key == "ArrowLeft"){
         gauche()
+        x = x + 1;
     }
     if (event.key == "ArrowRight"){
         droite()
+        x = x + 1;
     }
     if (event.key == "t" ){
         changeTitre("Nouveau titre")
+    }
+    if (event.key == "s" ){
+        
+        score()
     }
 }
 function changeTitre(Nouveau_titre){
     title= document.getElementsByTagName("h1");
     title[0].innerHTML = Nouveau_titre;
+}
+function score(){
+    console.log("score : ",x)
+    
 }
